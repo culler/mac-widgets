ef7e8021bcae05f3e86d24bc1c7023d681ec2c97
Marc Culler <marc.culler@gmail.com>
2019-11-30 10:22:13 -0600

    initial commit.

--- Widgets/ViewController.h
+++ Widgets/ViewController.h
@@ -10,6 +10,37 @@
 
 @interface ViewController : NSViewController
 
+@property (weak) IBOutlet NSProgressIndicator *topProgress;
+
+@property (weak) IBOutlet NSProgressIndicator *bottomProgress;
+
+@property (weak) IBOutlet NSSliderCell *topSlider;
+
+@property (weak) IBOutlet NSSliderCell *bottomSlider;
+
+@property (weak) IBOutlet NSProgressIndicator *indefinitePBar;
+
+@property (weak) IBOutlet NSButtonCell *checkAnimate;
+
+@property (weak) IBOutlet NSButtonCell *examplePushButton;
+
+@property (weak) IBOutlet NSButtonCell *checkDisableButtons;
+@property (weak) IBOutlet NSPopUpButtonCell *examplePopUpButton;
+@property (weak) IBOutlet NSButtonCell *exampleCheckBox;
+@property (weak) IBOutlet NSButtonCell *exampleRadio1;
+@property (weak) IBOutlet NSButtonCell *exampleRadio2;
+@property (weak) IBOutlet NSButtonCell *exampleTriangle;
+@property (weak) IBOutlet NSButtonCell *exampleImageButton;
+@property (weak) IBOutlet NSButtonCell *exampleRoundRect;
+@property (weak) IBOutlet NSButtonCell *exampleRecessedButton;
+@property (weak) IBOutlet NSButtonCell *exampleInlineButton;
+@property (weak) IBOutlet NSButtonCell *exampleTexturedButton;
+@property (weak) IBOutlet NSButtonCell *exampleGradientButton;
+@property (weak) IBOutlet NSButtonCell *exampleDisclosureButton;
+@property (weak) IBOutlet NSButtonCell *exampleHelpButton;
+
+- (IBAction)disableAllButtons:(id)sender;
+
 
 @end
 
--- Widgets/ViewController.m
+++ Widgets/ViewController.m
@@ -12,7 +12,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     // Do any additional setup after loading the view.
 }
 
@@ -23,5 +22,44 @@
     // Update the view, if already loaded.
 }
 
+- (IBAction)radio:(id)sender {
+    // All radio buttons with the same action form a group.
+}
 
+- (IBAction)topAction:(NSSliderCell *)sender {
+    self.topProgress.doubleValue = self.topSlider.doubleValue;
+}
+
+- (IBAction)bottomAction:(NSSliderCell *)sender {
+    self.bottomProgress.doubleValue = self.bottomSlider.doubleValue;
+}
+
+- (IBAction)startPBar:(NSButtonCell *)sender {
+    self.indefinitePBar.usesThreadedAnimation = false;
+    if ([sender state] == NSControlStateValueOn) {
+        [self.indefinitePBar startAnimation:sender];
+        NSLog(@"Started animation");
+    } else {
+        [self.indefinitePBar stopAnimation:sender];
+        NSLog(@"Stopped animation");
+    }
+}
+- (IBAction)disableAllButtons:(id)sender {
+    Boolean enable = [sender state] == NSControlStateValueOn ?
+    NO : YES;
+    [self.examplePushButton setEnabled:enable];
+    [self.examplePopUpButton setEnabled:enable];
+    [self.exampleCheckBox setEnabled:enable];
+    [self.exampleRadio1 setEnabled:enable];
+    [self.exampleRadio2 setEnabled:enable];
+    [self.exampleTriangle setEnabled:enable];
+    [self.exampleImageButton setEnabled:enable];
+    [self.exampleRoundRect setEnabled:enable];
+    [self.exampleRecessedButton setEnabled:enable];
+    [self.exampleInlineButton setEnabled:enable];
+    [self.exampleTexturedButton setEnabled:enable];
+    [self.exampleGradientButton setEnabled:enable];
+    [self.exampleDisclosureButton setEnabled:enable];
+    [self.exampleHelpButton setEnabled:enable];
+}
 @end
